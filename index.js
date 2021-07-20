de846d7107546c174aa2d99f4e23433bf7f69420
TaylorHo <taylorho.ta@gmail.com>
2021-07-20 10:06:44 -0300

    Listando os primeiros resultados da pesquisa

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const puppeteer = require("puppeteer");
 
-const getInfo = async () => {
-  const browser = await puppeteer.launch({ headless: true });
+(async () => {
+
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(
-    "https://www.google.com/search?q=unialcance&sxsrf=ALeKk01pCMFJcZ4GC5je3azk2YFWL5n6zg%3A1626185826798&ei=YqDtYOebMIXY5OUP-euzoAU&oq=unialcance&gs_lcp=Cgdnd3Mtd2l6EAxKBAhBGABQAFgAYIMCaABwAngAgAEAiAEAkgEAmAEAqgEHZ3dzLXdpesABAQ&sclient=gws-wiz&ved=0ahUKEwjn1K2knuDxAhUFLLkGHfn1DFQQ4dUDCA4"
-  );
-  const arrayDeDados = await pageEvaluate(() => {
-    //Toda funçao será executada no browser
+  await page.goto('https://www.google.com/search?q=unialcance')
+
+  const retorno = await page.evaluate(() => {
 
     const nodeList = document.querySelectorAll(".yuRUbf > a");
-
     const siteArray = [...nodeList];
 
     const list = siteArray.map((item) => {
       return {
         link: item.href,
-        title: item.querySelector("h3.LC20lb DKV0Md").innerText,
-      };
-    });
+        title: item.querySelector("h3.LC20lb.DKV0Md").innerText
+      }
+    })
 
     return list;
-  });
-  return arrayDeDados
-  
-};
-console.log( await getInfo )
+
+  })
+
+  console.log( await retorno );
+
+  await browser.close()
+
+})()
